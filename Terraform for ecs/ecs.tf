6ee139ed8dd3d719ec18af2cf5a45b11998a3d3c
Hrishit <hrishitghildiyal123@gmail.com>
2025-06-19 19:37:44 +0530

    Added logConfiguration to ECS task definition

--- Terraform for ecs/ecs.tf
+++ Terraform for ecs/ecs.tf
@@ -1,3 +1,8 @@
+resource "aws_cloudwatch_log_group" "strapi" {
+  name              = "/ecs/strapi-logs"
+  retention_in_days = 7
+}
+
 resource "aws_ecs_task_definition" "strapi_task" {
   family                   = "strapi-task"
   network_mode             = "awsvpc"
@@ -6,20 +11,26 @@ resource "aws_ecs_task_definition" "strapi_task" {
   memory                   = "1024"
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
-  container_definitions = jsonencode([
-    {
-      name      = "strapi"
-      image     = "hrishit2/strapi-app:latest"
-      essential = true
-      portMappings = [
-        {
-          containerPort = 1337
-          hostPort      = 1337
-          protocol      = "tcp"
-        }
-      ]
+  container_definitions = jsonencode([{
+    name      = "strapi"
+    image     = "hrishit2/strapi-app:latest"
+    essential = true
+    portMappings = [
+      {
+        containerPort = 1337
+        hostPort      = 1337
+        protocol      = "tcp"
+      }
+    ],
+    logConfiguration = {
+      logDriver = "awslogs"
+      options = {
+        awslogs-group         = aws_cloudwatch_log_group.strapi.name
+        awslogs-region        = "ap-south-1"
+        awslogs-stream-prefix = "strapi"
+      }
     }
-  ])
+  }])
 }
 
 resource "aws_ecs_service" "strapi_service" {
